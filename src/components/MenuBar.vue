19ed6274869230ee12269c7992584e600d90d407
LongShotPlaya <J.Nixon@eagles.oc.edu>
2023-10-24 13:09:02 -0500

    Loggout function and route done

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -3,6 +3,9 @@ import ocLogo from "/oc-logo-white.png";
 import { ref, onMounted } from "vue";
 import Utils from "../config/utils";
 import AuthServices from "../services/authServices";
+import { useRouter } from "vue-router";
+
+const router = useRouter ();
 
 const user = ref(null);
 const title = ref("Tutorials");
@@ -24,7 +27,7 @@ const logout = () => {
     .then((response) => {
       console.log(response);
       Utils.removeItem("user");
-      $router.push({ name: "login" });
+      router.push({ name: "login" });
     })
     .catch((error) => {
       console.log("error", error);
