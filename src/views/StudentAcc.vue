5b496816cb6d78dbfc4eebaf295eeb5c6a2443e8
LongShotPlaya <J.Nixon@eagles.oc.edu>
2023-10-31 10:11:05 -0500

    Startup for Student ACC Page

--- src/views/StudentAcc.vue
+++ src/views/StudentAcc.vue
@@ -1,9 +1,51 @@
 <script setup>
+import { ref, onMounted } from "vue";
+import studentAccomServices from "../services/studentAccomServices.js";
+import { useRouter } from "vue-router";
+
+const router = useRouter ();
+const message = ref("");
+
+const props = defineProps({
+  id: {
+    required: true,
+  },
+});
+
+const acc = ref({
+	metaData: null,
+	id: "",
+});
+
+
 
 </script>
 
 <template>
+    <v-container>
+		<v-toolbar>
+			<v-toolbar-title>Student {{ firstName }} {{ lastName }}'s accomodations</v-toolbar-title>
+		</v-toolbar>
+			<br>
+		<div class="container">
+			<v-form ref="form" v-model="valid" lazy validation>
+				<v-text-field 
+					required v-model="courses.name" id="name" label="Name">
+				</v-text-field>
 
+
+				<v-text-field 
+					required placeholder="ie. CMSC-1111" v-model="courses.courseNo" id="courseNo" label="Course Number">
+				</v-text-field>
+
+
+				<v-card-actions>
+					<v-btn :disabled=saveDisabled color="success" class="mr-4" @click="approve">Approve</v-btn>
+					<v-btn :disabled=updateDisabled color="success" class="mr-4" @click="deny">Deny</v-btn>
+				</v-card-actions>
+			</v-form>
+		</div>
+	</v-container>
 </template>
 
 <style>
