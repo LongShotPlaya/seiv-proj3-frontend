b8eb46b788fad0e15ad93d83046a57e87b3c9650
WilsonTrin <wilsontrin45@gmail.com>
2023-11-06 20:54:13 -0600

    Updating Request button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,7 @@ import { useRouter } from "vue-router";
 import Utils from "../config/utils.js";
 import SemesterServices from "../services/semesterServices.js";
 import RequestServices from "../services/requestServices.js";
+import StudentAccomServices from "../services/studentAccomServices.js";
 import UserServices from "../services/userServices.js";
 
 const router = useRouter();
@@ -13,20 +14,19 @@ const authTitle = ref("You should not see this")
 const firstHeader = ref("You should not see this");
 const secondHeader = ref("You should not see this");
 const thirdHeader = ref("You should not see this");
-const semesterTitle = ref("Select A Semester Please");
 
 const view = ref(-1);
 var thirdHead = false;
 var tableEnable = true;
 const user = Utils.getStore("user");
 
-const semesterid = ref(1);
+const semester = ref({name: "Please select a semester", id: -1});
 const semesters = ref([]);
-const test = ref(["A","B","C"]);
 
-const titleChange = async () => {
-	semesterTitle.value = "Hello World!";
-};
+//const adminTable = ref([]);
+const student = ref([]);
+const accommodation = ref([]);
+const request = ref([]);
 
 const authenTitle = async () => {
 	try {
@@ -70,25 +70,59 @@ const authenTitle = async () => {
 
 const makeRequest = () => {
 	const data = {
-    	studentId: user.id,
-		semesterId: semesterid,
+    	userId: user.userId,
+		semesterId: semester.value.id,
 		status: "Pending",
 		requestDate: Date(),
   	};
-	RequestServices.createRequest(data)
-    .then((response) => {
-	  request.value.id = response.data.id;
-	  message.value = "Request Created!";
-    })
-    .catch((e) => {
-      message.value = e.response.data.message;
-    });
+	console.log(data.semesterId)
+	if (semester.value == null)
+		message.value = "Please select a semester!";
+	else
+	{
+		RequestServices.createRequest(data)
+		.then((response) => {
+		//request.value.id = response.data.id;
+		message.value = "Request Created!";
+		})
+		.catch((e) => {
+		message.value = e.response.data.message;
+		});
+	}
 };
 
 const retrieveSemesters = () => {
     SemesterServices.getAllSemesters()
         .then((response) => {
           semesters.value = response.data;
+        })
+        .catch((e) => {
+          message.value = e.response.data.message;
+        });
+};
+
+const setAdminTable = () => {
+	// for loop through all students
+	// get student (can be multiple students)
+	// get the accommodation
+	// get the accommodation status
+    StudentAccomServices.getAllStudentAccomodations()
+        .then((response) => {
+          accommodation.value = response.data;
+        })
+        .catch((e) => {
+          message.value = e.response.data.message;
+        });
+	UserServices.getAllUsers()
+        .then((response) => {
+          student.value = response.data;
+        })
+        .catch((e) => {
+          message.value = e.response.data.message;
+        });
+	RequestServices.getAllRequests()
+        .then((response) => {
+          request.value = response.data;
         })
         .catch((e) => {
           message.value = e.response.data.message;
@@ -108,11 +142,12 @@ onMounted(() => {
 			<v-toolbar-title>{{ authTitle }}</v-toolbar-title>
 		</v-toolbar>
 		<br />
-		<div class="container">
-			<h2> Accommodations For {{ semesterTitle }}</h2>
-			<v-combobox label="Semester" v-model="semesterTitle" :items="test" @click="titleChange">
+		<div v-if=tableEnable class="container">
+			<h2> Accommodations</h2><!-- For {{ semesterTitle }}</h2> -->
+			<v-combobox label="Semester" v-model="semester" :items="semesters"
+			item-title="name">
 			</v-combobox>
-			<v-table v-if=tableEnable>
+			<v-table>
 				<thead>
 					<tr>
 						<th class="text-left"> {{ firstHeader }}</th>
@@ -121,11 +156,11 @@ onMounted(() => {
 					</tr>
 				</thead>
 				<tbody>
-				<!-- <tr v-for="(item, index) in adminTable" :key="index">
-					<td>{{ item.student }}</td>
-					<td>{{ item.accommodation }}</td>
-					<td v-if=thirdHead class="text-left">{{ item.status }}</td>
-				</tr> -->
+				<tr v-if=thirdHead>
+					<td>{{ student.name }}</td>
+					<td>{{ accommodation.name }}</td>
+					<td>{{ request.status }}</td>
+				</tr>
 				</tbody>
 			</v-table>
 			<v-btn color="success" class="mr-4" @click="makeRequest">
@@ -134,6 +169,8 @@ onMounted(() => {
 		</div>
 		<br />
       		<h4>{{ message }}</h4>
+			<h4>semester id: {{ semester.id }}</h4>
+			<h4>user id: {{ user.userId }}</h4>
       	<br />
 	</v-container>
 </template>
