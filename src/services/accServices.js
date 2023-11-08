e7981cb55b0360f80ac1ce46c1eecde3f7d02e36
Vyryty <colonelsuperawesome@gmail.com>
2023-11-07 20:34:41 -0600

    feat: Accommodation request page largely done
    I don't yet have the ability to generate a pdf or send notifications, but I think the basic functionality is there.

--- src/services/accServices.js
+++ src/services/accServices.js
@@ -10,11 +10,17 @@ export default {
   createAccomodation(data) {
     return apiClient.post(`/accomodations`, data);
   },
+  createAccomodations(data) {
+    return apiClient.post(`/accomodations/mult`, data);
+  },
   updateAccomodation(accomodationId, data) {
     return apiClient.put(`/accomodations/${accomodationId}`, data);
   },
   deleteAccomodation(accomodationId) {
     return apiClient.delete(`/accomodations/${accomodationId}`);
+  },
+  deleteAccomodations(data) {
+    return apiClient.delete(`/accomodations/mult`, data);
   },
   deleteAllAccomodations() {
     return apiClient.delete(`/accomodations`);
--- src/views/RequestDetails.vue
+++ src/views/RequestDetails.vue
@@ -1,7 +1,6 @@
 <script setup>
 import Utils from "../config/utils.js";
 import RequestServices from "../services/requestServices";
-import StudentServices from "../services/studentServices";
 import StudentAccomServices from "../services/studentAccomServices";
 import AccomServices from "../services/accServices";
 import UserServices from "../services/userServices";
@@ -23,21 +22,20 @@ const props = defineProps({
 });
 const message = ref("Add, Edit or Delete Lessons");
 
-const userRole = ref("Student");
+const userRole = Utils.getStore("user")?.role ?? "Student";
 const request = ref({});
 const student = ref({});
-const studentAccom = ref(null);
+const studentAccom = ref({});
 const semester = ref({});
 const accoms = ref([]);
 const selAccoms = ref([]);
 const accomCats = ref([]);
 
+// Gets list of all accommodation categories
 const refreshACats = () => {
     ACatServices.getAllAccomodationCats()
         .then((response) => {
             accomCats.value = response.data;
-            if (userRole.value == "Administrator") 
-                accoms.value = accomCats.value.map(cat => { return { ...cat, pdf: null }; })
         })
         .catch((err) => {
             console.log(err)
@@ -45,22 +43,50 @@ const refreshACats = () => {
         });
 };
 
+// Gets all accommodations for student
 const refreshAccoms = () => {
     AccomServices.getAllAccomodations()
         .then((response) => {
-            let data = response.data.filter(accom => accom.studentAccomId == studentAccom.value.id);
-
+            const data = response.data.filter(accom => accom.studentAccomId == studentAccom.value.id);
+            
             data.forEach(item => {
-                // Search for existing v-datatable row
-                // If does exist, update pdf value
-                // If doesn't exist, make a new one
+                const obj = {
+                    id: item.id,
+                    accomCatId: item.accomodationCatId,
+                    accomCat: accomCats.value.find(cat => cat.id == item.accomodationCatId).name,
+                    name: item.name,
+                    description: accomCats.value.find(cat => cat.id == item.accomodationCatId).description,
+                    pdf: item.pdf,
+                    exists: true,
+                };
+                accoms.value.push(obj);
+                selAccoms.value.push(obj);
             });
+
+            // If the user is an admin, allow them to see all options for accommodations
+            if (userRole == "Administrator")
+            {
+                accomCats.value.filter(cat => !accoms.value
+                    .find(accom => accom.accomCatId == cat.id))
+                    .forEach(cat => {
+                        accoms.value.push({
+                            id: null,
+                            accomCatId: cat.id,
+                            accomCat: cat.name,
+                            name: cat.name,
+                            description: cat.description,
+                            pdf: null,
+                            exists: false,
+                        })
+                    });
+            }
         })
         .catch((err) => {
             message.value = err.response.data.message;
         });
 };
 
+// Gets the request which was sent in
 const refreshRequest = () => {
     RequestServices.getRequest(props.requestId)
         .then((response) => {
@@ -71,10 +97,7 @@ const refreshRequest = () => {
         });
 };
 
-const refreshRole = () => {
-    userRole.value = Utils.getStore("user").role;
-};
-
+// Gets the semester for this request
 const refreshSemester = () => {
     SemesterServices.getSemester(request.value.semesterId)
         .then((response) => {
@@ -85,8 +108,8 @@ const refreshSemester = () => {
         });
 };
 
+// Gets the student who the request is for
 const refreshStudent = () => {
-    //StudentServices.getStudent(props.studentId)
     UserServices.getUser(props.studentId)
         .then((response) => {
             student.value = response.data;
@@ -96,6 +119,7 @@ const refreshStudent = () => {
         });    
 };        
 
+// Gets the student accommodation for the student in the specified semester
 const refreshStudentAccom = () => {
     SemesterServices.getStudentAccomsForSemester(semester.value.id)
         .then((response) => {
@@ -106,69 +130,104 @@ const refreshStudentAccom = () => {
         });
 };    
 
-const onSaveRequest = () => {
+// Saves the new request and updates the accommodations
+const onSaveRequest = async () => {
+    // Updates the status of the request
     request.value.status = selAccoms.value.length > 0 ? "Accepted" : "Declined";
 
+    // Update request
     RequestServices.updateRequest(props.requestId, request.value)
         .then((response) => {
             
         })
         .catch((err) => {
             message.value = err.response.data.message;
-        })
+        });
+    
+    // Make a new student accommodation if it doesn't already exist and if the request was accepted
+    if (!studentAccom.value && request.value.status == "Accepted")
+    {
+        // Make new student accommodation object
+        const newSA = {
+            userId: student.value.id,
+            semesterId: semester.value.id,
+            metaData: null,
+        };
+        // Wait for student accommodation to be created before continuing
+        await StudentAccomServices.createStudentAccomodation(newSA)
+            .then((response) => {
+                studentAccom.value = response.data;
+            })
+            .catch((err) => {
+                message.value = err.response.data.message;
+            })
+    }
 
-    // Add new accoms if in selAccoms, but not in studentAccoms
-    let newAccoms = selAccoms.value
-        .filter(accom => !studentAccom.value.find(sAccom => sAccom.accomodationId == accom.id))
-        .map(accom => {
+    // Add new accoms if in selAccoms, but doesn't exist yet
+    const newAccoms = selAccoms.value
+        .filter(selAccom => !selAccom.exists)
+        .map(selAccom => {
             return {
-                accomodationId: accom.id,
-                studentId: student.value.id,
-                semesterId: semester.value.id,
-                metaData: null
+                name: selAccom.name,
+                pdf: selAccom.pdf,
+                studentAccomId: studentAccom.value.id,
+                accomodationCatId: selAccom.accomCatId,
             };
         });
     
+    // If there are accommodations to add, add them
     if (newAccoms.length > 0)
     {
-        StudentAccomServices.createStudentAccomodations(newAccoms)
-            .then((response) => {
-
-            })
-            .catch((err) => {
-                message.value = err.response.data.message;
-            });
+        console.log("Adding:")
+        console.log(newAccoms)
+        //AccomServices.createAccomodations(newAccoms)
+        //    .then((response) => {
+        //
+        //    })
+        //    .catch((err) => {
+        //        message.value = err.response.data.message;
+        //    });
     }
     
-    // Delete accom if not in selAccoms, but in studentAccoms
-    let deletedAccoms = studentAccom.value
-        .filter(sAccom => !selAccoms.value.find(accom => accom.id == sAccom.accomodationId))
-        .map(sAccom => sAccom.id);
+    // Delete accom if not in selAccoms, but in accoms
+    const deletedAccoms = accoms.value
+        .filter(accom => accom.exists && !selAccoms.value.find(selAccom => selAccom.id == accom.id))
+        .map(accom => accom.id);
     
+    // If there are accommodations to be deleted, delete them
     if (deletedAccoms.length > 0)
     {
-        StudentAccomServices.deleteStudentAccomodations(deletedAccoms)
-            .then((response) => {
-        
-            })
-            .catch((err) => {
-                message.value = err.response.data.message;
-            });
+        console.log("Deleting:")
+        console.log(deletedAccoms)
+        //AccomServices.deleteAccomodations(deletedAccoms)
+        //    .then((response) => {
+        //
+        //    })
+        //    .catch((err) => {
+        //        message.value = err.response.data.message;
+        //    });
     }
+
+    refreshAll();
 }
 
+// Does nothing except navigate the user back to the home page
 const onCancel = () => {
     router.push({ name: "home" });
 };
 
-onMounted(() => {
-    refreshRole();
+// Refreshes everything that needs to be
+const refreshAll = () => {
     refreshRequest();
     refreshStudent();
     refreshSemester();
     refreshACats();
     refreshStudentAccom();
     refreshAccoms();
+};
+
+onMounted(() => {
+    refreshAll();
 });
 </script>
 
@@ -181,7 +240,9 @@ onMounted(() => {
             <v-card-title>Requested On: {{ `${request.requestDate}` }}</v-card-title>
             <v-card-title>Request Status: {{ request.status }}</v-card-title>
             <br>
-            <v-card-title>Accommodations:</v-card-title>
+            <v-card-title
+                v-if="userRole == 'Administrator' || request.status == 'Accepted'"
+            >Accommodations:</v-card-title>
             <v-data-table
                 v-if="userRole == 'Administrator' || request.status == 'Accepted'"
                 v-model="selAccoms"
@@ -191,7 +252,7 @@ onMounted(() => {
                     { title: 'PDF Link', align: 'start', key: 'pdf' }
                 ]"
                 :items="accoms"
-                :sort-by="[{ key: 'name', order: 'asc' }]"
+                :sort-by="[{ key: 'accomCat', order: 'asc' }]"
                 :items-per-page="10"
                 return-object
                 select-strategy="multiple"
@@ -201,10 +262,18 @@ onMounted(() => {
         <br>
         <v-row justify="center">
             <v-col cols="1">
-                <v-btn color="secondary" @click="onSaveRequest" v-if="userRole == 'Administrator'">Save</v-btn>
+                <v-btn
+                    v-if="userRole == 'Administrator'"
+                    color="secondary"
+                    :disabled="new Date(semester.endDate) > new Date()"
+                    @click="onSaveRequest"
+                >Save</v-btn>
             </v-col>
             <v-col cols="1">
-                <v-btn color="secondary" @click="onCancel">{{ userRole == 'Administrator' ? "Cancel" : "Back" }}</v-btn>
+                <v-btn
+                    color="primary"
+                    @click="onCancel"
+                >{{ userRole == 'Administrator' ? "Cancel" : "Back" }}</v-btn>
             </v-col>
         </v-row>
     </v-container>
